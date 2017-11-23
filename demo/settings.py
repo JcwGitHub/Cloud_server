12738566f30b367293a10923c40d7e51a5e05172
JcwGitHub <772763832@qq.com>
2017-11-24 00:27:00 +0800

    no message

--- demo/settings.py
+++ demo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'm+8@0kna^t-#0sb45uv8n^_jz%e9myq(i2=oi)h_dn94$z&e@s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
